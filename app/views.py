ab3bd8d581b900ab3061caafb0f23d9bc85b9520
mic1000 <tmicahl@hotmail.com>
2015-06-28 09:34:13 +0200

    First commit

---
+++ app/views.py
@@ -0,0 +1,37 @@
+from flask import render_template, flash, redirect
+from app import app
+from .forms import LoginForm
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'nickname': 'Miguel'}  # fake user
+    posts = [
+        { 
+            'author': {'nickname': 'John'}, 
+            'age' : '25',
+            'body': 'Beautiful day in Portland!' 
+        },
+        { 
+            'author': {'nickname': 'Susan'},
+            'age' : '85', 
+            'body': 'The Avengers movie was so cool!' 
+        }
+    ]
+    return render_template('index.html', 
+                            title='home', 
+                            user=user,
+                            posts=posts)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login request for OpenID="%s"' %
+            (form.openid.data))
+        return redirect('/index')
+    return render_template('login.html', 
+                           title='Sign In',
+                           form=form,
+                           providers=app.config['OPENID_PROVIDERS'])
